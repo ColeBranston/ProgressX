749515f6b9f13f392d33a548f39195dc46241bfd
Cole Branston <colebranston@gmail.com>
2026-02-15 17:07:35 -0500

    added test files for searching and updating fields

---
+++ data/testing/TEST_check_doc_exists.py
@@ -0,0 +1,29 @@
+from solr_instance import *
+from datetime import datetime, timezone
+
+now = str(datetime.now(timezone.utc).isoformat().replace("+00:00", "Z"))
+
+id_var = "This is the id"
+
+results = solr_clean_core.search(f'id:"{id_var}"', rows=1)
+
+if results.hits >= 1:
+    print("Documents found updating UPDATED_AT")
+    solr_clean_core.add([
+        {
+            "id": f"{id_var}",
+            "content": {"set": "This is the updated content body"},
+            "updated_at": {"set": now}
+        }
+    ])
+
+else:
+    print("No document exists, adding new doc to core")
+    solr_clean_core.add([
+        {
+            "id": f"{id_var}",
+            "content" : "This is the content body where main abstracts will be stored",
+            "created_at": now,
+            "updated_at": now
+        }
+    ])
--- data/testing/solr_instance.py
+++ data/testing/solr_instance.py
@@ -6,7 +6,7 @@ env = solrConfigs["env"]
 print(solrConfigs[env]+"raw_ingestion_data")
 
 solr_raw_core = pysolr.Solr(
-    solrConfigs[env]+"raw_ingestion_data", timeout=10)
+    solrConfigs[env]+"raw_ingestion_data/", timeout=10)
 
 solr_clean_core = pysolr.Solr(
-    solrConfigs[env]+"clean_ingestion_data", timeout=10)
+    solrConfigs[env]+"clean_ingestion_data/", timeout=10)
