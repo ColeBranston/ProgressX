fc1097bab23c78bb06b372c2a9a6e6e49b77ff24
ColeBranston <118498961+ColeBranston@users.noreply.github.com>
2026-03-19 03:17:17 -0400

    added frontend cache support, and path params support for saving queries and proper navigation

---
+++ progressx/src/app/(main)/following/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+export default function FollowingPage() {
+
+    return(
+        <div className="mainWrapper">
+            <p>This is the following page</p>
+        </div>
+    )
+}
