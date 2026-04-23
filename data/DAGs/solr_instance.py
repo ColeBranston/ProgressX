94bf11ec66bbe19573ee81344c653ccb9421b4f3
ColeBranston <118498961+ColeBranston@users.noreply.github.com>
2026-04-23 00:28:40 -0400

    added improved timeouts for connection abort error

--- data/DAGs/solr_instance.py
+++ data/DAGs/solr_instance.py
@@ -6,16 +6,16 @@ env = solrConfigs["env"]
 print(solrConfigs[env]+"raw_ingestion_data")
 
 solr_raw_core = pysolr.Solr(
-    solrConfigs[env]+"raw_ingestion_data/")
+    solrConfigs[env]+"raw_ingestion_data/",  timeout=200)
 
 solr_clean_core = pysolr.Solr(
-    solrConfigs[env]+"clean_ingestion_data/")
+    solrConfigs[env]+"clean_ingestion_data/",  timeout=200)
 
 def reignite_core(core_name: str):
     match core_name:
         case "raw":
-            return pysolr.Solr(solrConfigs[env]+"raw_ingestion_data/")
+            return pysolr.Solr(solrConfigs[env]+"raw_ingestion_data/", timeout=200)
         case "clean":
-            return pysolr.Solr(solrConfigs[env]+"clean_ingestion_data/")
+            return pysolr.Solr(solrConfigs[env]+"clean_ingestion_data/", timeout=200)
         case _:
-            raise Exception("Reignite Function Error: Core Not Found")
+            raise Exception("Reignite Function Error: Core Not Found", timeout=200)
