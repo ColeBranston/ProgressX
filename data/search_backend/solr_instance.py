eeac0e77cfe67241645db9fc4f7dda70bbc0ad1c
ColeBranston <118498961+ColeBranston@users.noreply.github.com>
2026-04-22 22:26:14 -0400

    removed timeout for solr connection

--- data/search_backend/solr_instance.py
+++ data/search_backend/solr_instance.py
@@ -4,7 +4,7 @@ from config import solrConfigs
 env = solrConfigs["env"]
 
 solr_raw_core = pysolr.Solr(
-    solrConfigs[env]+"raw_ingestion_data/", timeout=10)
+    solrConfigs[env]+"raw_ingestion_data/")
 
 solr_clean_core = pysolr.Solr(
-    solrConfigs[env]+"clean_ingestion_data/", timeout=10)
+    solrConfigs[env]+"clean_ingestion_data/")
